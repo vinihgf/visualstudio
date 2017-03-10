f362bab6851ba249455a106813c1789bcf7f4c2d
Vinicius <vinicius_gf_@hotmail.com>
2017-03-10 10:52:08 -0300

    criei um projeto de exemplo

---
+++ DIRETIVA/DIRETIVA/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DIRETIVA.Startup))]
+namespace DIRETIVA
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
